0f40c16bbe1b94b6900d4baa44e80ce27d5e51c8
Gihan Shyavinda Jayathissa <jayathissakdgs.21@uom.lk>
2024-12-14 16:29:48 +0530

    .....

--- src/app/shared/cards/book-card/book-card.component.ts
+++ src/app/shared/cards/book-card/book-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { BookService } from '../book-card/book.service';
 
 @Component({
   selector: 'app-book-card',
@@ -6,6 +7,9 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrl: './book-card.component.css',
 })
 export class BookCardComponent implements OnInit {
+
+ 
+  @Input() userId!: string;
   goToPreview(arg0: any) {
     throw new Error('Method not implemented.');
   }
@@ -13,10 +17,23 @@ export class BookCardComponent implements OnInit {
     throw new Error('Method not implemented.');
   }
   @Input() book: any;
-  constructor() {}
+  constructor( private bookService:BookService) {}
 
   ngOnInit(): void {
     console.log('Book page');
     console.log('Books:', this.book);
   }
+  addCart(book: any) {
+
+    this.bookService.addItem(this.userId, book.book_id) // Replace '1' with the actual user_id
+      .subscribe(
+        response => {
+          console.log('Item added to cart:', response);
+        },
+        error => {
+          console.error('Error adding item to cart:', error);
+        }
+      );
+
+}
 }
