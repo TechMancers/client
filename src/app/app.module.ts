07fe54da0f4c0f298281df089b687bdb7c67fb2d
Jeewani Saranga <155546436+JeewaniSaranga99@users.noreply.github.com>
2024-12-14 16:31:35 +0530

    fggg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { St02Component } from './pages/sign-up/sign-up-users/st02/st02.component
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
-import { HTTP_INTERCEPTORS} from '@angular/common/http';
+
 
 import { HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AuthInterceptor } from './shared/services/auth.interceptor';
